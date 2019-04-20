30cfb990627419e049ca9bcf1409fca675086fa2
Ryan Magoon <mr.magoon5@gmail.com>
2019-04-19 21:08:10 -0400

    sorted letters for list headers

--- app/src/modules/ContactList.js
+++ app/src/modules/ContactList.js
@@ -2,12 +2,25 @@ import React from 'react'
 
 import ContactListItem from './ContactListItem'
 
-const ContactList = ({ contacts }) => (
-  <aside>
-    {contacts.map(contact => (
-      <ContactListItem key={contact.id} {...contact} />
-    ))}
-  </aside>
-)
+const ContactList = ({ contacts }) => {
+  const lettersForSortedContactList = [
+    ...new Set(contacts.map(({ name }) => name.charAt(0)))
+  ]
+    .sort()
+    .reduce(
+      (letters, nextLetter) => ({
+        ...letters,
+        [nextLetter]: []
+      }),
+      {}
+    )
 
+  return (
+    <aside>
+      {contacts.map(contact => (
+        <ContactListItem key={contact.id} {...contact} />
+      ))}
+    </aside>
+  )
+}
 export default ContactList
