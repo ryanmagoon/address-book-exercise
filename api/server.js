13f77bf5120301f8fc5130c85dacc443132f94d2
Ryan Magoon <mr.magoon5@gmail.com>
2019-04-19 20:44:02 -0400

    allow CORS for my dev server

--- api/server.js
+++ api/server.js
@@ -1,39 +1,42 @@
-require('colors');
-const path = require('path');
-const express = require('express');
+require('colors')
+const path = require('path')
+const express = require('express')
+var cors = require('cors')
 
-const peopleBlob = require(path.join(__dirname, 'data/people.json'));
-const people = peopleBlob['people'];
+const peopleBlob = require(path.join(__dirname, 'data/people.json'))
+const people = peopleBlob['people']
 
-const app = express();
-app.get('/api/people/:id', getPersonById);
-app.get('/api/people', getAllPeople);
+const app = express()
+app.get('/api/people/:id', cors(), getPersonById)
+app.get('/api/people', cors(), getAllPeople)
 
-const HTTP_PORT = 8080;
-app.listen(HTTP_PORT, function (err) {
-    if (err) {
-        throw err;
-    }
+const HTTP_PORT = 8080
+app.listen(HTTP_PORT, function(err) {
+  if (err) {
+    throw err
+  }
 
-    console.log(('HTTP server listening on port ' + HTTP_PORT).green);
-    console.log('People data:'.bold + ' http://localhost:' + HTTP_PORT + '/api/people');
-});
+  console.log(('HTTP server listening on port ' + HTTP_PORT).green)
+  console.log(
+    'People data:'.bold + ' http://localhost:' + HTTP_PORT + '/api/people'
+  )
+})
 
 function getPersonById(req, res) {
-    const id = parseInt(req.params.id, 10);
-    const filtered = people.filter(function (person) {
-        return person.id === id;
-    });
+  const id = parseInt(req.params.id, 10)
+  const filtered = people.filter(function(person) {
+    return person.id === id
+  })
 
-    if (filtered.length === 0) {
-        res.sendStatus(404);
-    } else {
-        res.json({
-            person: filtered[0]
-        });
-    }
+  if (filtered.length === 0) {
+    res.sendStatus(404)
+  } else {
+    res.json({
+      person: filtered[0]
+    })
+  }
 }
 
 function getAllPeople(req, res) {
-    res.json(peopleBlob);
+  res.json(peopleBlob)
 }
