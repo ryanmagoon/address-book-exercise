fb40550431c12856043b98b94621bb13c7eeae95
Ryan Magoon <mr.magoon5@gmail.com>
2019-04-19 21:25:09 -0400

    sorted side list with headers

--- app/src/modules/ContactList.js
+++ app/src/modules/ContactList.js
@@ -3,22 +3,31 @@ import React from 'react'
 import ContactListItem from './ContactListItem'
 
 const ContactList = ({ contacts }) => {
-  const lettersForSortedContactList = [
+  const SortedContactList = [
     ...new Set(contacts.map(({ name }) => name.charAt(0)))
   ]
     .sort()
     .reduce(
       (letters, nextLetter) => ({
         ...letters,
-        [nextLetter]: []
+        [nextLetter]: contacts
+          .filter(({ name }) => name.charAt(0) === nextLetter)
+          .sort((a, b) => (a.name < b.name ? -1 : 1))
       }),
       {}
     )
 
+  console.log(SortedContactList)
+
   return (
     <aside>
-      {contacts.map(contact => (
-        <ContactListItem key={contact.id} {...contact} />
+      {Object.keys(SortedContactList).map(letter => (
+        <section key={letter}>
+          <h2>{letter}</h2>
+          {SortedContactList[letter].map(contact => (
+            <ContactListItem key={contact.id} {...contact} />
+          ))}
+        </section>
       ))}
     </aside>
   )
--- app/src/modules/ContactListItem.js
+++ app/src/modules/ContactListItem.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ContactListItem = ({ name }) => (
   <section>
-    <h2>{name}</h2>
+    <h3>{name}</h3>
   </section>
 )
 
