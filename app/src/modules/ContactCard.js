d82f4cf66f63aa234565124824193696d53b4b3e
Ryan Magoon <mr.magoon5@gmail.com>
2019-04-20 20:58:11 -0400

    style container, make it scrollable, and plug in the data into the new components

--- app/src/modules/ContactCard.js
+++ app/src/modules/ContactCard.js
@@ -1,15 +1,21 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import ContactInfo from './ContactInfo'
-const Education = () => null
-const Experience = () => null
+import Education from './Education'
+import Experience from './Experience'
 
-const ContactCard = ({ education, experience, ...info }) => (
-  <article>
+const Container = styled.article`
+  height: 100vh;
+  overflow-y: scroll;
+`
+
+const ContactCard = ({ education, workExperience, ...info }) => (
+  <Container>
     <ContactInfo {...info} />
     <Education {...education} />
-    <Experience {...experience} />
-  </article>
+    <Experience {...workExperience} />
+  </Container>
 )
 
 export default ContactCard
